542f7aed7947175a56c92d3727ac17bab9721cd2
mconrejas <mconrejas@gmail.com>
2019-09-17 22:07:52 +0800

    New end point

--- server.js
+++ server.js
@@ -18,4 +18,16 @@ app.post('/api/transact', function (req, res) {
     });
 })
 
+app.get('/api/account', function (req, res) {
+    let id = req.query.id;
+
+    // Lazily import the assets-listing function.
+    const { account } = require("./src/account");
+
+    // Log the result to the console.
+    return account({ id }).then(data => {
+        res.json(data)
+    });
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
